c176ba25a98cf952165b11a10f02e2c4b7d59006
aubreyjcook <aubreyjcook@gmail.com>
2018-04-16 15:49:00 -0500

    setting up jlist

--- src/Lab10.java
+++ src/Lab10.java
@@ -1,13 +1,41 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.SwingUtilities;
 
-public class Lab10 {
-
+public class Lab10 extends JFrame{
+	private JList<String> selectionList;
+	
 	public Lab10() {
-		// TODO Auto-generated constructor stub
+		DefaultListModel<String> listModel = new DefaultListModel<>();
+		listModel.addElement("Math");
+		listModel.addElement("Computer");
+		listModel.addElement("Physics");
+		listModel.addElement("Chemistry");
+		
+		selectionList = new JList<>(listModel);
+		add(selectionList);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("JList Example");
+		this.setSize(200,200);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
 	}
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Lab10();
+			}
+		});
 	}
+	
+	
 
 }
